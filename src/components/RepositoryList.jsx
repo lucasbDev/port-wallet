4ead9f31b7ab13a78ef4493e08fdfb79a359e723
Lucas Barbosa <luccacpm@gmail.com>
2022-10-17 23:51:14 -0300

    sevt change

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,19 @@
+const RepositoryName = 'first';
+
+export function RepositoryList() {
+    return (
+        <section className="Repository-list">
+            <h1>lista de Repository</h1>
+
+            <ul>
+                <li>
+                    <strong>{RepositoryList}</strong>
+                        <p> forms in react</p>
+                        <a href="">
+                            filadelphia
+                        </a>
+                </li>
+            </ul>
+        </section>
+    )
+}
